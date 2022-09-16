87ab4ec0afc07675f6dc74a2987be653991ea5a6
Luca Zampetti <lzampetti@gmail.com>
2022-09-16 12:20:06 +0200

    main

--- apps/web/src/pages/api/auth/forgot/index.ts
+++ apps/web/src/pages/api/auth/forgot/index.ts
@@ -2,7 +2,7 @@ import { apiHandler } from '@websolute/core';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default apiHandler({
-  post: (request: NextApiRequest, response: NextApiResponse<null>) => {
+  post: async (request: NextApiRequest, response: NextApiResponse<null>) => {
     const body = request.body;
     console.log('POST > api/auth/forgot', body.email);
     response.status(200).json(null);
--- apps/web/src/pages/api/auth/register/index.ts
+++ apps/web/src/pages/api/auth/register/index.ts
@@ -2,7 +2,7 @@ import { apiHandler } from '@websolute/core';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default apiHandler({
-  post: (request: NextApiRequest, response: NextApiResponse<null>) => {
+  post: async (request: NextApiRequest, response: NextApiResponse<null>) => {
     const body = request.body;
     console.log('POST > api/auth/register', body);
     response.status(200).json(null);
