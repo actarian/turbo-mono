518ac8c4be6eb9d8519204081d8b5b614db45fc3
Luca Zampetti <lzampetti@gmail.com>
2022-11-25 15:58:32 +0100

    store-api

--- packages/store/src/store-api/store-api.store.ts
+++ packages/store/src/store-api/store-api.store.ts
@@ -11,10 +11,25 @@ export async function getApiStore<T extends IStore>(pages: { [key: string]: stri
   // console.log('getApiStore');
   const store: { [key: string]: IQuerable<IEntity> } = {};
   const keys = Object.keys(pages);
-  const routes = [...keys, 'page', 'route', 'country', 'label', 'locale', 'market', 'menu'];
+  const routes = [...keys,
+    'category',
+    'country',
+    'feature_type',
+    'label',
+    'list',
+    'locale',
+    'market',
+    'page',
+    'province',
+    'region',
+    'route',
+    'template',
+  ];
+  routes.sort();
   routes.forEach(key => {
     store[key] = new StoreApiService<any>(key);
   });
+  console.log(store);
   STORE_ = store;
   return STORE_ as T;
 }
